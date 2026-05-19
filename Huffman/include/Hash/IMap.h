eac431afb75a22a51471d0cfacb4f06afa8b4473
Happymath09 <90992242+BlueWhaleo5@users.noreply.github.com>
2026-05-19 09:33:50 +0700

    Update IMap.h

--- Huffman/include/Hash/IMap.h
+++ Huffman/include/Hash/IMap.h
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   IMap.h
- * Author: LTSACH
- *
  * Created on 22 August 2020, 21:53
  */
 
