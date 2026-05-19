60be09d8674ad917a178838fc220e8337070fe54
Happymath09 <90992242+BlueWhaleo5@users.noreply.github.com>
2026-05-19 09:38:43 +0700

    Update IList.h

--- Huffman/include/list/IList.h
+++ Huffman/include/list/IList.h
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   IList.h
- * Author: LTSACH
- *
  * Created on 13 August 2020, 13:54
  */
 
